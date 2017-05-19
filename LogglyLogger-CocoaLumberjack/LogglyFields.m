b6df7678150d9c3c2363f679ec9de938064b4f54
Peter Gulyas <peter@datinc.ca>
2017-05-19 10:06:58 -0700

    Modified for OSX

--- LogglyLogger-CocoaLumberjack/LogglyFields.m
+++ LogglyLogger-CocoaLumberjack/LogglyFields.m
@@ -4,7 +4,19 @@
 
 #import "LogglyFields.h"
 
+
+
+#if TARGET_OS_IPHONE
 @import UIKit;
+#elif TARGET_OS_MAC
+@import Cocoa;
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
+
+#endif
 
 @implementation LogglyFields {
     dispatch_queue_t _queue;
@@ -29,14 +41,44 @@
         if(bundleVersion != nil) {
             [dict setObject:bundleVersion forKey:@"appversion"];
         }
+
+#if TARGET_OS_IPHONE
         [dict setObject:[UIDevice currentDevice].name forKey:@"devicename"];
         [dict setObject:[UIDevice currentDevice].model forKey:@"devicemodel"];
         [dict setObject:[UIDevice currentDevice].systemVersion forKey:@"osversion"];
+#elif TARGET_OS_MAC
+        [dict setObject:[[NSHost currentHost] localizedName] forKey:@"devicename"];
+        [dict setObject:[self machineModel] forKey:@"devicemodel"];
+        [dict setObject:[self machineModel] forKey:@"osversion"];
+#endif
         [dict setObject:[self generateRandomStringWithSize:10] forKey:@"sessionid"];
         _fieldsDictionary = [NSDictionary dictionaryWithDictionary:dict];
     }
     return self;
 }
+
+#if TARGET_OS_MAC
+-(NSString *) machineModel {
+    size_t len = 0;
+    sysctlbyname("hw.model", NULL, &len, NULL, 0);
+
+    if (len) {
+        char *model = malloc(len*sizeof(char));
+        sysctlbyname("hw.model", model, &len, NULL, 0);
+        NSString *model_ns = [NSString stringWithUTF8String:model];
+        free(model);
+        return model_ns;
+    }
+
+    return @"(empty)";
+}
+
+-(NSString *) osVersion {
+    NSOperatingSystemVersion version = [[NSProcessInfo processInfo] operatingSystemVersion];
+    NSString* versionStr = [NSString stringWithFormat:@"%ld.%ld.%ld", (long)version.majorVersion, (long)version.majorVersion, (long)version.patchVersion];
+    return versionStr;
+}
+#endif
 
 #pragma mark implementation of LogglyFieldsDelegate protocol
 
